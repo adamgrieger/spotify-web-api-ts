aaa40db686d93e88d8f537e8945e813de267a592
Adam Grieger <adamgrieger@gmail.com>
2020-07-21 17:19:59 -0700

    docs(episodesapi.ts): add code examples and required scopes

--- src/apis/EpisodesApi.ts
+++ src/apis/EpisodesApi.ts
@@ -11,10 +11,20 @@ export class EpisodesApi {
   }
 
   /**
-   * Get an Episode
+   * ### Get an Episode
    *
    * Get Spotify catalog information for a single episode identified by its
    * unique Spotify ID.
+   *
+   * **Required Scopes:** `user-read-playback-position` for reading the user's
+   * resume points.
+   *
+   * @example
+   * ```ts
+   * const episode = await spotify.episodes.getEpisode('5VhaAEtTrKQ1NSneHsXK6C');
+   * console.log(episode.name);
+   * // "S1E6 - Institutionalized by Kendrick Lamar"
+   * ```
    *
    * @param options Optional request information.
    */
@@ -26,10 +36,23 @@ export class EpisodesApi {
   }
 
   /**
-   * Get Several Episodes
+   * ### Get Several Episodes
    *
    * Get Spotify catalog information for multiple episodes based on their
    * Spotify IDs.
+   *
+   * **Required Scopes:** `user-read-playback-position` for reading the user's
+   * resume points.
+   *
+   * @example
+   * ```ts
+   * const episodes = await spotify.episodes.getEpisodes([
+   *  '3Ht9rzglNNgJBFGp2RL7o2',
+   *  '7r40piSdCht1NL720dKNWf',
+   * ]);
+   * console.log(episodes.map(episode => episode.name));
+   * // Array [ "The Soul Train Episode", "How the Voyager Golden Records Work" ]
+   * ```
    *
    * @param episodeIds A list of the Spotify IDs for the episodes.
    * @param options Optional request information.
