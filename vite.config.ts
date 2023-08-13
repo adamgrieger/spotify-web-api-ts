61d0c937cd1e7b1bf9d43eb5eb65c9641ca7e3d8
inferrinizzard <inferrinizzard@gmail.com>
2023-08-13 16:59:06 -0700

    build with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from "vitest/config"
+import { resolve } from 'path';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	cacheDir: './node_modules/.vite',
 
+	build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'spotify-web-api',
+      fileName: 'spotify-web-api',
+    },
+  },
+
 	test: {
-		globals:true,
+		globals: true,
 		cache: {
 			dir: './node_modules/.vitest'
 		},
 		environment: 'jsdom',
 		include: ['src/**/*.test.{js,mjs,cjs,ts,mts,cts}'],
-		typecheck: {tsconfig: './tsconfig.test.json'}
+		typecheck: { tsconfig: './tsconfig.test.json' }
 	}
 })
