ac30f76a5561778cd06b873af58a1fb42810fcab
inferrinizzard <inferrinizzard@gmail.com>
2023-08-14 11:30:46 -0700

    move schema to dir

--- tools/fetch-schema.sh
+++ tools/fetch-schema.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl https://developer.spotify.com/reference/web-api/open-api-schema.yaml > ./tools/openapi-schema.yaml
-curl https://developer.spotify.com/reference/policies.yaml > ./tools/policies.yaml
+curl https://developer.spotify.com/reference/web-api/open-api-schema.yaml > ./tools/schema/main/openapi-schema.yaml
+curl https://developer.spotify.com/reference/policies.yaml > ./tools/schema/policies.yaml
