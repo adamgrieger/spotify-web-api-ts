c7ea23040a29389943d779ac32917e004ba32f23
inferrinizzard <inferrinizzard@gmail.com>
2023-08-14 07:32:03 -0700

    lint vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,32 @@
 import { resolve } from 'path';
-import { defineConfig } from "vitest/config";
+
+import { defineConfig } from 'vitest/config';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-	cacheDir: './node_modules/.vite',
+  cacheDir: './node_modules/.vite',
 
-	build: {
+  build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'spotify-web-api',
       fileName: 'spotify-web-api',
     },
   },
-	plugins: [dts({
-		tsconfigPath: 'tsconfig.build.json',
-		rollupTypes: true,
-		insertTypesEntry: true,
-	})],
+  plugins: [
+    dts({
+      tsconfigPath: 'tsconfig.build.json',
+      rollupTypes: true,
+    }),
+  ],
 
-	test: {
-		globals: true,
-		cache: {
-			dir: './node_modules/.vitest'
-		},
-		environment: 'jsdom',
-		include: ['src/**/*.test.{js,mjs,cjs,ts,mts,cts}'],
-		typecheck: { tsconfig: './tsconfig.test.json' }
-	}
-})
+  test: {
+    globals: true,
+    cache: {
+      dir: './node_modules/.vitest',
+    },
+    environment: 'jsdom',
+    include: ['src/**/*.test.{js,mjs,cjs,ts,mts,cts}'],
+    typecheck: { tsconfig: './tsconfig.test.json' },
+  },
+});
