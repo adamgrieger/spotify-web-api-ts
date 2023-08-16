cdb6af38a84715f6614fbce8148a7370c1114611
inferrinizzard <inferrinizzard@gmail.com>
2023-08-16 10:42:06 -0700

    remove contentType from spotifyAxios

--- src/main/spotifyAxios.ts
+++ src/main/spotifyAxios.ts
@@ -1,35 +1,26 @@
-import axios, {
-  type AxiosInstance,
-  type AxiosRequestConfig,
-  type RawAxiosRequestHeaders,
-} from 'axios';
-
-export type SpotifyAxiosConfig = AxiosRequestConfig & {
-  contentType?: RawAxiosRequestHeaders['Content-Type'];
-};
+import axios, { type AxiosInstance, type AxiosRequestConfig } from 'axios';
 
 let spotifyAxios: SpotifyAxios;
 
 export class SpotifyAxios {
   public axiosInstance: AxiosInstance;
 
-  public constructor(config?: SpotifyAxiosConfig) {
+  public constructor(config?: AxiosRequestConfig) {
     this.axiosInstance = axios.create(config);
 
     this.axiosInstance.interceptors.request.use((req) => {
       const request = req;
-      request.headers['Content-Type'] =
-        config?.contentType ?? 'application/json';
 
       return request;
     });
+
     this.axiosInstance.interceptors.response.use((res) => {
       return res;
     });
   }
 }
 
-export const getSpotifyAxios = (config?: SpotifyAxiosConfig): SpotifyAxios => {
+export const getSpotifyAxios = (config?: AxiosRequestConfig): SpotifyAxios => {
   if (!spotifyAxios) {
     spotifyAxios = new SpotifyAxios(config);
   }
