704075bd9aa2de30e7c14ecbe346ecf40b015c93
inferrinizzard <inferrinizzard@gmail.com>
2023-08-16 10:59:25 -0700

    passThrough request.ts

--- src/openapi/core/request.ts
+++ src/openapi/core/request.ts
@@ -1,1 +1,33 @@
-export * from './_request';
+import axios, { type AxiosInstance } from 'axios';
+
+import { type CancelablePromise } from './CancelablePromise';
+import { type ApiRequestOptions } from './ApiRequestOptions';
+import { request as rawRequest } from './_request';
+import { type OpenAPIConfig } from './OpenAPI';
+
+export {
+  base64,
+  catchErrorCodes,
+  getFormData,
+  getHeaders,
+  getQueryString,
+  getRequestBody,
+  getResponseBody,
+  getResponseHeader,
+  isBlob,
+  isDefined,
+  isFormData,
+  isString,
+  isStringWithValue,
+  isSuccess,
+  resolve,
+  sendRequest,
+} from './_request';
+
+export const request = <T>(
+  config: OpenAPIConfig,
+  options: ApiRequestOptions,
+  axiosClient: AxiosInstance = axios,
+): CancelablePromise<T> => {
+  return rawRequest(config, options, axiosClient);
+};
