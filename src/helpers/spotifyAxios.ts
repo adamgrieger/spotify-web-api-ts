06087f2649f0fb33ff9114f6de2821b66152e080
inferrinizzard <inferrinizzard@gmail.com>
2023-08-16 07:41:57 -0700

    remove old spotifyAxios

--- src/helpers/spotifyAxios.ts
+++ src/helpers/spotifyAxios.ts
@@ -3,44 +3,10 @@ import axios, {
   type AxiosRequestConfig,
   type RawAxiosRequestHeaders,
 } from 'axios';
-import qs from 'qs';
-
-// import { BASE_API_URL } from '../constants';
 
 export type SpotifyAxiosConfig = AxiosRequestConfig & {
   contentType?: RawAxiosRequestHeaders['Content-Type'];
 };
-
-// export async function spotifyAxios<T>(
-//   url: string,
-//   method: Method,
-//   accessToken: string,
-//   config?: SpotifyAxiosConfig,
-// ): Promise<T> {
-//   try {
-//     const { contentType, ...axiosConfig } = config ?? {};
-//     const response = await axios({
-//       ...axiosConfig,
-//       baseURL: BASE_API_URL,
-//       headers: {
-//         'Authorization': `Bearer ${accessToken}`,
-//         'Content-Type': contentType ?? 'application/json',
-//       },
-//       paramsSerializer,
-//       url,
-//       method,
-//     });
-
-//     return response.data as T;
-//   } catch (error) {
-//     const err = error as AxiosError;
-//     throw new Error(err.message);
-//   }
-// }
-
-export function paramsSerializer(params: unknown): string {
-  return qs.stringify(params, { arrayFormat: 'comma' });
-}
 
 let spotifyAxios: SpotifyAxios;
 
