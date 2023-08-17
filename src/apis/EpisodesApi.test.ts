15fb57ef51a1f0029fbef9048f6ba7a1d9a1a6f9
inferrinizzard <inferrinizzard@gmail.com>
2023-08-16 23:51:32 -0700

    restore tests for EpisodesApi

--- src/apis/EpisodesApi.test.ts
+++ src/apis/EpisodesApi.test.ts
@@ -1,87 +1,62 @@
-import { type MockedClass } from 'vitest';
+import { type SpyInstance } from 'vitest';
 
 import { episodeFixture, getEpisodesFixture } from '../fixtures';
-import { Http } from '../helpers/Http';
+import { EpisodesService } from '../openapi/services/EpisodesService';
 
 import { EpisodesApi } from './EpisodesApi';
 
-vi.mock('../helpers/Http');
-
-const HttpMock = Http as MockedClass<typeof Http>;
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-function setup() {
-  const httpMock = new HttpMock('token');
-  const episodes = new EpisodesApi();
-
-  return { httpMock, episodes };
-}
+const episodes = new EpisodesApi();
 
 describe('EpisodesApi', () => {
-  beforeEach(() => {
+  afterEach(() => {
     vi.resetAllMocks();
   });
 
   describe('getEpisode', () => {
+    let getEpisodeSpy: SpyInstance;
     beforeEach(() => {
-      HttpMock.prototype.get.mockResolvedValue(episodeFixture);
+      getEpisodeSpy = vi
+        .spyOn(EpisodesService, 'getAnEpisode')
+        .mockResolvedValue(episodeFixture);
     });
 
-    it.todo('should get an episode (without options)', async () => {
-      const { httpMock, episodes } = setup();
-
+    it('should get an episode (without options)', async () => {
       const response = await episodes.getEpisode('foo');
 
       expect(response).toEqual(episodeFixture);
-      expect(httpMock.get).toHaveBeenCalledWith('/episodes/foo', undefined);
+      expect(getEpisodeSpy).toHaveBeenCalledWith('foo', undefined);
     });
 
-    it.todo('should get an episode (with options)', async () => {
-      const { httpMock, episodes } = setup();
-
+    it('should get an episode (with options)', async () => {
       const response = await episodes.getEpisode('foo', { market: 'bar' });
 
       expect(response).toEqual(episodeFixture);
-      expect(httpMock.get).toHaveBeenCalledWith('/episodes/foo', {
-        params: {
-          market: 'bar',
-        },
-      });
+      expect(getEpisodeSpy).toHaveBeenCalledWith('foo', 'bar');
     });
   });
 
   describe('getEpisodes', () => {
+    let getEpisodesSpy: SpyInstance;
     beforeEach(() => {
-      HttpMock.prototype.get.mockResolvedValue(getEpisodesFixture);
+      getEpisodesSpy = vi
+        .spyOn(EpisodesService, 'getMultipleEpisodes')
+        .mockResolvedValue(getEpisodesFixture);
     });
 
-    it.todo('should get several episodes (without options)', async () => {
-      const { httpMock, episodes } = setup();
-
+    it('should get several episodes (without options)', async () => {
       const response = await episodes.getEpisodes(['foo', 'bar']);
 
       expect(response).toEqual(getEpisodesFixture.episodes);
-      expect(httpMock.get).toHaveBeenCalledWith('/episodes', {
-        params: {
-          ids: ['foo', 'bar'],
-        },
-      });
+      expect(getEpisodesSpy).toHaveBeenCalledWith('foo,bar', undefined);
     });
 
-    it.todo('should get several episodes (with options)', async () => {
-      const { httpMock, episodes } = setup();
-
+    it('should get several episodes (with options)', async () => {
       const response = await episodes.getEpisodes(['foo', 'bar'], {
         market: 'baz',
       });
 
       expect(response).toEqual(getEpisodesFixture.episodes);
-      expect(httpMock.get).toHaveBeenCalledWith('/episodes', {
-        params: {
-          ids: ['foo', 'bar'],
-          market: 'baz',
-        },
-      });
+      expect(getEpisodesSpy).toHaveBeenCalledWith('foo,bar', 'baz');
     });
   });
 });
