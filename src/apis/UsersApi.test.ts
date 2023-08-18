7f35560d74ea8c4845eb3c5f74124cc863c054f1
inferrinizzard <inferrinizzard@gmail.com>
2023-08-18 01:39:29 -0700

    restore tests for UsersApi

--- src/apis/UsersApi.test.ts
+++ src/apis/UsersApi.test.ts
@@ -1,54 +1,36 @@
-import { type MockedClass } from 'vitest';
-
 import { privateUserFixture, publicUserFixture } from '../fixtures';
-import { Http } from '../helpers/Http';
+import { UsersService } from '../openapi/services/UsersService';
 
 import { UsersApi } from './UsersApi';
 
-vi.mock('../helpers/Http');
-
-const HttpMock = Http as MockedClass<typeof Http>;
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-function setup() {
-  const httpMock = new HttpMock('token');
-  const users = new UsersApi();
-
-  return { httpMock, users };
-}
+const users = new UsersApi();
 
 describe(UsersApi.name, () => {
-  beforeEach(() => {
+  afterEach(() => {
     vi.resetAllMocks();
   });
 
   describe('getMe', () => {
-    beforeEach(() => {
-      HttpMock.prototype.get.mockResolvedValue(privateUserFixture);
-    });
-
-    it.todo("should get the current user's profile", async () => {
-      const { httpMock, users } = setup();
-
+    it("should get the current user's profile", async () => {
+      const getMeSpy = vi
+        .spyOn(UsersService, 'getCurrentUsersProfile')
+        .mockResolvedValue(privateUserFixture);
       const response = await users.getMe();
 
       expect(response).toEqual(privateUserFixture);
-      expect(httpMock.get).toHaveBeenCalledWith('/me');
+      expect(getMeSpy).toHaveBeenCalledWith();
     });
   });
 
   describe('getUser', () => {
-    beforeEach(() => {
-      HttpMock.prototype.get.mockResolvedValue(publicUserFixture);
-    });
-
-    it.todo("should get a user's profile", async () => {
-      const { httpMock, users } = setup();
-
+    it("should get a user's profile", async () => {
+      const getUserSpy = vi
+        .spyOn(UsersService, 'getUsersProfile')
+        .mockResolvedValue(publicUserFixture);
       const response = await users.getUser('foo');
 
       expect(response).toEqual(publicUserFixture);
-      expect(httpMock.get).toHaveBeenCalledWith('/users/foo');
+      expect(getUserSpy).toHaveBeenCalledWith('foo');
     });
   });
 });
